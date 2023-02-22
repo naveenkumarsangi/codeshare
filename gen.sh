49e811f2975c84edbcaacf9945d53d9ca6ffa97b
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2023-02-22 17:08:49 +0530

    gen.sh: Add script to generate keys

---
+++ gen.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function file_exists() {
+    [ -f "$1" ] && echo "file already exists: $1" && exit 1
+}
+
+file_exists "$HOME/.ssh/codeshare.pem"
+file_exists "$HOME/.ssh/codeshare.public.pem"
+
+openssl genrsa -aes128 -passout pass:codeshare -out private.pem 4096
+openssl rsa -in private.pem -passin pass:codeshare -pubout -out public.pem
+
+mv private.pem ~/.ssh/codeshare.pem
+mv public.pem ~/.ssh/codeshare.public.pem
+
+echo "Please copy the following public key into api/index.go"
+cat ~/.ssh/codeshare.public.pem
+
